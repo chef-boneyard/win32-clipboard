50b9b7cdc8d84301e6e89e797996996b25aa84f7
Daniel Berger <djberg96@gmail.com>
2014-01-09 07:03:03 -0700

    Updated html clipboard tests.

--- test/test_html_clipboard.rb
+++ test/test_html_clipboard.rb
@@ -3,9 +3,10 @@
 #
 # Test suite for the HtmlClipboard class.
 ########################################################################
-gem 'test-unit'
-require 'test/unit'
+require 'test-unit'
+require 'win32/clipboard'
 require 'win32/html_clipboard'
+include Win32
 
 class TC_Html_Clipboard < Test::Unit::TestCase
   def setup
@@ -13,30 +14,30 @@ class TC_Html_Clipboard < Test::Unit::TestCase
     @plain_str = "Writing to the clipboard is easy with this code"
   end
 
-  def test_cf_html_constant
-    assert_not_nil(Win32::HtmlClipboard::CF_HTML)
+  test "CF_HTML constant is defined" do
+    assert_not_nil(HtmlClipboard::CF_HTML)
     assert_kind_of(Integer, HtmlClipboard::CF_HTML)
   end
 
-  def test_get_data_basic
+  test "get_data basic functionality" do
     assert_respond_to(HtmlClipboard, :get_data)
   end
 
-  def test_set_data_basic
+  test "set_data basic functionality" do
     assert_respond_to(HtmlClipboard, :set_data)
   end
 
-  def test_set_and_get_data
+  test "getting html data returns a plain string" do
     assert_nothing_raised{ HtmlClipboard.set_data(@html_str) }
     assert_equal(@plain_str, HtmlClipboard.get_data)
   end
 
-  def test_is_html_format
+  test "html_format basic functionality" do
     assert_respond_to(HtmlClipboard, :html_format?)
     assert_boolean(HtmlClipboard.html_format?)
   end
 
-  def test_data_details_basic
+  test "data details basic functionality" do
     assert_respond_to(HtmlClipboard, :data_details)
     assert_nothing_raised{ HtmlClipboard.data_details }
     assert_kind_of(String, HtmlClipboard.data_details)
